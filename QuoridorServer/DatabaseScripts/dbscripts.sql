6e24adf3a0ade19549fc92747a59555ba0c92a07
User <User@LAB3-8>
2021-10-25 15:01:49 +0300

    Commit on sharyn's orders

--- QuoridorServer/DatabaseScripts/dbscripts.sql
+++ QuoridorServer/DatabaseScripts/dbscripts.sql
@@ -6,12 +6,33 @@ Use QuoridorDB
 Go
 
 
-Create Table Users (
-ID int Identity primary key,
+Create Table GameMoves(
+GameMoveID int identity primary key,
+GameMoveGameID int not null foreign key references Games(GameID),
+GameMovePlayerID int not null foreign key references Players(PlayerID),
+SecondsFromStart int not null, -- time spent since the start (excluding this move) of the game for this payer ONLY
+-- this value of the first move for a player is, of course 0.
+MoveTime int not null, -- the time this move took
+IsPawnMove bit not null, -- tells if it's a pawn move or a block placement
+int x, -- new x-value of the player / block placement
+int y, -- new y-value of the player / block placemt
+BlockDirection char, -- if it is a block placement, tels the drection. Can include: 'U', 'D', 'L', 'R'
+)
+
+Create Table Games (
+GameID int identity primary key,
+GameMoves nvarchar(300) not null,
+GameTimeStamps nvarchar(300) not null,
+
+)
+
+Create Table Players (
+PlayerID int identity primary key,
 Email nvarchar(100) not null,
+Username nvarchar(100) not null,
 FirstName nvarchar(30) not null,
 LastName nvarchar(30) not null,
-UserPswd nvarchar(30) not null,
+PlayerPass nvarchar(30) not null,
 CONSTRAINT UC_Email UNIQUE(Email)
 )
 
