72cd5c644b704fd1103c1654dec60827d269fc0d
yoavl <yoavl0904@gmail.com>
2021-10-27 14:55:05 +0300

    added QuoridorServerBL and wrote SignIn

---
+++ QuoridorServerBL/ModelsBL/QuoridorDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuoridorServerBL.Models
+{
+    public partial class QuoridorDBContext
+    {
+
+        /*
+        Searches for a player with matching username and password and returns it.
+        If no such player exists - returns null.
+        */
+        public Player SignIn(string username, string password)
+        {
+            var query = from p in Players where (p.UserName == username && p.PlayerPass == password) select p;
+            Player pQuery = query.FirstOrDefault();
+            return pQuery;
+        }
+
+    }
+}
